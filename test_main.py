101b16dc7a439c707508505fa6d3bad28584cf17
pradeepaidtu <95844245+pradeepaidtu@users.noreply.github.com>
2022-01-18 16:43:18 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,11 +1,11 @@
 import main
 
 
-#def test_add():
-#    assert main.add(3, 4) == 7
-#    assert main.add(3.5, 4) == 7
-#    assert main.add(3.9, 4) == 7
-#    assert main.add(3.9, 4.1) == 8
+def test_add():
+    assert main.add(3, 4) == 7
+    assert main.add(3.5, 4) == 7
+    assert main.add(3.9, 4) == 7
+    assert main.add(3.9, 4.1) == 8
 
 
  def test_to_sentence():
